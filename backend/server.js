31929ebe74021f6162e05aa8223e14e0e927a190
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-05-12 16:06:21 -0300

    refactor: remove deprecated task management files and update database connection to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,13 @@ const port = 3000;
 
 app.use(express.json())
 app.use(cors());
-// TODO: check why this is not working
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT
-// })
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "todo_app",
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 connection.connect((error) => {
@@ -35,70 +27,56 @@ connection.connect((error) => {
 })
 
 app.get('/', (req, res) => {
-    const query = 'SELECT * FROM tasks;';
+    const query = 'SELECT * FROM tarefas;';
     connection.query(query, (err, results) => {
         if (err) {
-            console.error('Error trying to get results', err);
-            return res.status(500).json({ error: 'Error retrieving tasks' });
+            console.error('Erro ao tentar obter os resultados', err);
+            return res.status(500).json({ error: 'Erro ao recuperar tarefas' });
         }
         res.json(results);
     });
 });
 
 app.post('/addTask', (req, res) => {
-    const { title, isConclued } = req.body;
-    const query = 'INSERT INTO tasks (title, isConclued) VALUES (?, ?);';
+    const { tarefa, estaConcluida } = req.body;
+    const query = 'INSERT INTO tarefas (tarefa, estaConcluida) VALUES (?, ?);';
 
-    connection.query(query, [title, isConclued], (err, results) => {
+    connection.query(query, [tarefa, estaConcluida], (err, results) => {
         if (err) {
-            console.error('Error trying to insert task', err);
-            return res.status(500).json({ error: 'Error inserting task' });
+            console.error('Erro ao inserir nova tarefa.', err);
+            return res.status(500).json({ error: 'Erro ao inserir nova tarefa.' });
         }
-        res.json({ message: 'Task added successfully', taskId: results.insertId });
+        res.json({ message: 'tarefa adicionada com sucesso!', taskId: results.insertId });
     });
 });
 
 app.put('/updateTask/:id', (req, res) => {
     const {id} = req.params;
-    const { title, isConclued } = req.body;
-    const query = 'UPDATE tasks SET title = ?, isConclued = ? WHERE id = ?;';
+    const { tarefa, estaConcluida } = req.body;
+    const query = 'UPDATE tarefas SET tarefa = ?, estaConcluida = ? WHERE id = ?;';
 
-    connection.query(query, [title, isConclued, id], (err, results) => {
+    connection.query(query, [tarefa, estaConcluida, id], (err, results) => {
         if (err) {
-            console.error('Error trying to update task', err);
-            return res.status(500).json({ error: 'Error updating task' });
+            console.error('Erro ao atualizar a tarefa.', err);
+            return res.status(500).json({ error: 'Erro ao atualizar a tarefa.' });
         }
-        res.json({ message: 'Task updated successfully' });
+        res.json({ message: 'Tarefa atualizada com sucesso.' });
     });
 });
 
 app.delete('/deleteTask/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'DELETE FROM tasks WHERE id = ?;';
+    const query = 'DELETE FROM tarefas WHERE id = ?;';
 
     connection.query(query, [id], (err, results) => {
         if (err) {
-            console.error('Error trying to delete task', err);
-            return res.status(500).json({ error: 'Error deleting task' });
+            console.error('Erro ao tentar deletar a tarefa', err);
+            return res.status(500).json({ error: 'Erro ao tentar deletar a tarefa.' });
         }
-        res.json({ message: 'Task deleted successfully' });
+        res.json({ message: 'Tarefa deletada com sucesso.' });
     });
 })
 
-app.get('/countTasks', (req, res) => {
-    const query = 'SELECT COUNT(*) AS count FROM tasks';
-
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.error('Error trying to count tasks', err);
-            return res.status(500).json({ error: 'Error counting tasks' });
-        }
-
-        res.json({ count: results[0].count });
-    });
-});
-
-
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Servidor conectado! Servidor está rodando em: http://localhost:${port}`);
 });
